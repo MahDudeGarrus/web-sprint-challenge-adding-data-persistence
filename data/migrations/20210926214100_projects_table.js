58515685d3b768b7aef539fb99d1e63ad8a5b0b9
Your Name <you@example.com>
2021-09-26 15:58:01 -0600

    resources migration completed

--- data/migrations/20210926214100_projects_table.js
+++ data/migrations/20210926214100_projects_table.js
@@ -3,9 +3,9 @@ exports.up = function(knex) {
   return knex.schema
   .createTable('projects', table => {
       table.increments('project_id');
-      table.text('project_name', 128)
+      table.string('project_name', 128)
         .notNullable();
-      table.text('project_description', 256)
+      table.string('project_description', 256)
       table.boolean('project_completed')
         .defaultTo(false)
   })
--- data/migrations/20210926214145_resources_table.js
+++ data/migrations/20210926214145_resources_table.js
@@ -1,8 +1,14 @@
-
+//RESOURCES MIGRATION
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('resources', table => {
+      table.increments('resource_id')
+      table.string('resource_name', 128)
+        .notNullable()
+        .unique()
+      table.string('resource_description', 256)
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('resources')
 };
