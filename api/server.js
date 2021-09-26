b6da6eecb6cae99ee343c71eb489b30f1e34990d
Your Name <you@example.com>
2021-09-26 15:14:18 -0600

    server.js is built

--- api/server.js
+++ api/server.js
@@ -1,1 +1,21 @@
-// build your server here and require it from index.js
+const express = require('express')
+// WILL NEED ROUTERS LATER
+
+const server = express()
+
+server.use(express.json())
+
+//SET UP ROUTERS WITH USE METHOD
+
+//CATCH ALL
+server.use("*", (req, res, next) => {
+    next({ status: 404, message: 'not found'})
+})
+
+server.use((err, req, res, next) => { // eslint disable line
+    res.status(err.status || 500).json({
+        message: err.message
+    })
+})
+
+module.exports = server
