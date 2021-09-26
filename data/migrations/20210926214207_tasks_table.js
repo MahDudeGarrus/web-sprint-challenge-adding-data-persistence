589234fdd5ac8688dfd9cb5ef0bcce19c9b23f86
Your Name <you@example.com>
2021-09-26 16:10:17 -0600

    tasks migrations completed

--- data/migrations/20210926214207_tasks_table.js
+++ data/migrations/20210926214207_tasks_table.js
@@ -1,8 +1,21 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('tasks', table => {
+      table.increments('task_id')
+      table.string('task_description', 256)
+        .notNullable()
+      table.string('task_notes', 256)
+      table.boolean('task_completed')
+        .defaultTo(false)
+      table.integer('project_id')
+        .unsigned()
+        .references('project_id')
+        .inTable('projects')
+        .onDelete('RESTRICT')
+        .onUpdate('RESTRICT')
+  })
 };
 
 exports.down = function(knex) {
-  
+    return knex.scehma.dropTableIfExists('tasks');
 };
