0c3a182615a2e0a917ac9e9561c8d07d49cf5530
Your Name <you@example.com>
2021-09-26 17:55:17 -0600

    11 tests passing 7 failing

--- api/resource/model.js
+++ api/resource/model.js
@@ -2,11 +2,16 @@
 const db = require('../../data/dbConfig.js')
 
 function getAll(){
-
+    return db('resources')
+    .select('resources.*')
 }
 
-function create(){
-
+function create(resource){
+    return db('resources')
+    .insert(resource)
+    .then(([resource_id]) => {
+        return db('resources').where('resource_id', resource_id).first()
+    })
 }
 
 module.exports = {
--- api/task/model.js
+++ api/task/model.js
@@ -1,13 +1,18 @@
-// TASK MODEL
+// TASK MODEL!!
 
 const db = require('../../data/dbConfig.js')
 
 function getAll(){
-
+    return db('tasks')
+    .select('tasks.*')
 }
 
-function create(){
-
+function create(task){
+    return db('tasks')
+    .insert(task)
+    .then(([task_id]) => {
+        return db('tasks').where('task_id', task_id).first()
+    })
 }
 
 module.exports = {
